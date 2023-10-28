786a3cc20635a42aa802dbc1f48bdd979e4bf5ce
Jamila Malak <85511031+jmalak-27032k@users.noreply.github.com>
2023-10-28 12:59:17 +0530

    Update Test.py updated code with speech recognition

--- Test.py
+++ Test.py
@@ -1,7 +1,4 @@
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
-
+import tkinter as tk 
 from tkinter import Message, Text
 from tkinter.filedialog import askopenfile
 from tkinter.ttk import*
@@ -15,22 +12,59 @@ import time
 import shutil
 import threading
 import easyocr
-from PIL import Image
+from PIL import Image, ImageTk
 import tkinter.ttk as ttk 
 import tkinter.font as font 
 from pathlib import Path
 import face_recognition
 import re
-#import matplotlib
+import matplotlib
 from tkinter import messagebox
 import csv
+import speech_recognition as sr
 
-#List to store the uploaded file paths
-file_names = []
 
-popup_queue = []
 
-###########################
+def show_popup1():
+    popup = tk.Toplevel(window)
+    popup.title("MESSAGE")
+    x = window.winfo_rootx() + window.winfo_width() // 2 - 100
+    y = window.winfo_rooty() + window.winfo_height() // 2 - 50
+    popup.geometry(f"300x150+{x}+{y}")
+    tk.Label(popup, text="All files uploaded successfully. Thanks!", padx=20, pady=20).pack()
+    popup.after(2000, popup.destroy)
+    #tk.Button(popup, text="Close", command=popup.destroy).pack()
+
+def show_popup2():
+    popup = tk.Toplevel(window)
+    popup.title("MESSAGE")
+    x = window.winfo_rootx() + window.winfo_width() // 2 - 100
+    y = window.winfo_rooty() + window.winfo_height() // 2 - 50
+    popup.geometry(f"300x150+{x}+{y}")
+    tk.Label(popup, text="Please click on capture image. Thanks!", padx=20, pady=20).pack()
+    popup.after(2500, popup.destroy)
+
+def show_popup(title):
+    popup = tk.Toplevel(window)
+    popup.title("MESSAGE")
+    x = window.winfo_rootx() + window.winfo_width() // 2 - 100
+    y = window.winfo_rooty() + window.winfo_height() // 2 - 50
+    popup.geometry(f"300x150+{x}+{y}")
+    tk.Label(popup, text=title, padx=20, pady=20).pack()
+    tk.Button(popup, text="Close", command=popup.destroy).pack()
+
+
+    
+#function for browsing files
+def upload_docs():
+    #add line--21--09
+    file_path = askopenfile(mode='r',title="Select Files", filetypes=[('Image Files','.png;.jpg;.jpeg')])
+    if file_path:
+        file_name=file_path.name
+        print(file_name)
+        show_popup('File Uploaded Successfully!'f" Selected file path:\n{file_name}")
+        file_names.append(file_name)
+
 #function for uploading files
 def submit_docs():
     if not file_names:
@@ -82,9 +116,9 @@ def submit_docs():
             #add logic to extract aadhaar card fields and return 
             print("Extracting Aadhaar data")
             # Define a simple regular expression to capture names
-            name_pattern = r"[A-Z][a-z]+ [A-Z][a-z]+ [A-Z][a-z]+"
+            name_pattern = r'\b(?:[A-Z][a-z]+[-\s]?)+[A-Z][a-z]*\b'
             aadhar_pattern = r'^\b\d{4}\s\d{4}\s\d{4}\b$'
-            dob_pattern = r"\d{2}/\d{2}/\d{4}"
+            dob_pattern = r'\b(\d{1,2}[-/./]\d{1,2}[-/./]\d{4})\b'
             
             for detection in extracted_result:
                 text=detection[1]
@@ -123,7 +157,6 @@ def submit_docs():
     
     isAadhaarValid = checkAadhaarValidity(aadhaarNo, aadhaarName)
     RecogImg.config(state=tk.NORMAL)      #button will be enabled for recognition
-    show_popup3()
     #RecogImg.config(state=tk.NORMAL)  # button will be enabled for recognition
 
     retval_submit_docs=aadhaarName+","+aadhaarNo+","+aadhaarDob+","+str(isAadhaarValid)+","+panNo+","+passportNo
@@ -200,6 +233,8 @@ def process_document_image(uploaded_file_path,doc_position,destination_folder_ma
 
     except Exception as e:
         print("Error extracting image")
+    
+    show_popup2()
         
 #############################################################
         
@@ -392,60 +427,77 @@ def show_countdown_window(countdown_time):
 ### compare_images
 
 
-############UI started from here
-# Function to open file dialog for uploading documents
-def browse_file(label):
-    #file_path = filedialog.askopenfilename()
-    #label.config(text=file_path)
-    file_path = askopenfile(mode='r',title="Select Files", filetypes=[('Image Files','.png;.jpg;*.jpeg')])
-    if file_path:
-        file_name=file_path.name
-        print(file_name)
-        show_popup('File Uploaded Successfully!'f" Selected file path:\n{file_name}")
-        file_names.append(file_name)
+# Function to start speech recognition
+def start_speech_recognition():
 
-# Function to display a popup message
-def show_popup1():
-    popup = tk.Toplevel(root)
-    popup.title("Information Extracted")
-    popup.geometry("300x100")
-    tk.Label(popup, text="All files uploaded successfully. Thanks!", padx=20, pady=20).pack()
-    tk.Button(popup, text="Close", command=popup.destroy).pack()
-	
-def show_popup2():
-    popup = tk.Toplevel(root)
-    popup.title("Information Extracted")
-    popup.geometry("300x100")
-    tk.Label(popup, text="Please click on capture image. Thanks!", padx=20, pady=20).pack()
-    tk.Button(popup, text="Close", command=popup.destroy).pack()
-	
-def show_popup3():
-    popup = tk.Toplevel(root)
-    popup.title("Information Extracted")
-    popup.geometry("300x100")
-    tk.Label(popup, text="Please click on capture image. Thanks!", padx=20, pady=20).pack()
-    tk.Button(popup, text="Close", command=popup.destroy).pack()
+    start_speech_recognition()
+        # Initialize the recognizer
+    recognizer = sr.Recognizer()
 
-# Create the main window
-root = tk.Tk()
-root.title("KYC Process")
-root.geometry("800x600")
+
+    # Create a VideoCapture object to access the camera (0 is usually the built-in camera)
+    #cap = cv2.VideoCapture(0)
+ 
+    #while True:
+        # Read a frame from the camera
+        # ret, frame = cap.read()
+
+         #if not ret:
+            # break
+    # Display the frame in a window
+    #cv2.imshow('Camera Feed', frame)
+        
+    # Speech Recognition implementation 
+    with sr.Microphone() as source:
+        try:    
+            audio = recognizer.listen(source)
+            recognized_text = recognizer.recognize_google(audio)
+            print("You said: " + recognized_text)
+        except sr.UnknownValueError:
+            recognized_text ="Could not understand audio"
+            print(recognized_text)
+        except sr.RequestError as e:
+            recognized_text ="Error"
+            print(recognized_text)
+
+  
+
+    # Compare the strings (case-insensitive)
+    if recognized_text.lower() == text_recog.lower():
+        #show_popup("SPEECH RECOGNITION SUCCESSFUL !")
+        print("Strings are the same")
+        #cap.release()
+        #cv2.destroyAllWindows()
+    else:
+        #show_popup("SPEECH RECOGNITION UNSUCCESSFUL !")
+        print("Strings are different")
+        #cap.release()
+        #cv2.destroyAllWindows()
+
+
+    
+        
+
+#Name of the UI window
+window =tk.Tk()
+window.title("KYC Verification Process")
+window.geometry("800x600")
 
 # Center the main window on the screen
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 x_coordinate = (screen_width - 800) // 2
 y_coordinate = (screen_height - 600) // 2
-root.geometry(f"800x600+{x_coordinate}+{y_coordinate}")
+window.geometry(f"800x600+{x_coordinate}+{y_coordinate}")
 
 # Add a background image or backdrop (replace 'background_image.png' with your image)
 background_image = tk.PhotoImage(file="bg3.png")
-background_label = tk.Label(root, image=background_image)
+background_label = tk.Label(window, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
 # Create a frame for the content with a white background
-content_frame = tk.Frame(root, bg="#C6E2FF")
-content_frame.place(relx=0.13, rely=0.25, relwidth=0.35, relheight=0.5)
+content_frame = tk.Frame(window, bg="#C6E2FF")
+content_frame.place(relx=0.13, rely=0.25, relwidth=0.35, relheight=0.53)
 
 # Header Label
 heading_frame = tk.Frame(content_frame, bg="#C6E2FF",width=100, height=70)
@@ -457,63 +509,85 @@ logo_label.place(relwidth=1, relheight=1)
 #header_label.pack(pady=20)
 
 # Create frames for each row of components (Name, Aadhar Card, PAN Card, Passport)
-name_frame = tk.Frame(content_frame, bg="#C6E2FF")
-name_frame.pack(pady=10, padx=50, fill=tk.X)
+#name_frame = tk.Frame(content_frame, bg="#C6E2FF")
+#name_frame.pack(pady=10, padx=50, fill=tk.X)
 
-aadhar_frame = tk.Frame(content_frame, bg="#C6E2FF")
-aadhar_frame.pack(pady=10, padx=50, fill=tk.X)
+#aadhar_frame = tk.Frame(content_frame, bg="#C6E2FF")
+#aadhar_frame.pack(pady=10, padx=50, fill=tk.X)
 
-pan_frame = tk.Frame(content_frame, bg="#C6E2FF")
-pan_frame.pack(pady=10, padx=50, fill=tk.X)
+#pan_frame = tk.Frame(content_frame, bg="#C6E2FF")
+#pan_frame.pack(pady=10, padx=50, fill=tk.X)
 
-passport_frame = tk.Frame(content_frame, bg="#C6E2FF")
-passport_frame.pack(pady=10, padx=50, fill=tk.X)
+#passport_frame = tk.Frame(content_frame, bg="#C6E2FF")
+#passport_frame.pack(pady=10, padx=50, fill=tk.X)
 
 # Labels and Entry/Upload Buttons in each row
-name_label = tk.Label(name_frame, text="Name", font=("Helvetica", 14),bg="#C6E2FF")
+name_label = tk.Label(window, text="Name", font=("Helvetica", 14),bg="#C6E2FF")
 name_label.pack(side=tk.LEFT, padx=10)
-name_txt = tk.Entry(name_frame, font=("Helvetica", 14))
+name_label.place(x = 250, y = 305)
+name_txt = tk.Entry(window, font=("Helvetica", 14))
 name_txt.pack(side=tk.RIGHT, padx=10, fill=tk.X, expand=True)
+name_txt.place(x = 400, y = 310)
 
-aadhar_label = tk.Label(aadhar_frame, text="Aadhar Card*", font=("Helvetica", 14), bg="#C6E2FF")
+aadhar_label = tk.Label(window, text="Aadhar Card", font=("Helvetica", 14), bg="#C6E2FF")
 aadhar_label.pack(side=tk.LEFT, padx=10)
-aadhar_file_label = tk.Label(aadhar_frame, text="", font=("Helvetica", 12), bg="#C6E2FF")
-aadhar_file_label.pack(side=tk.LEFT, padx=10)
-aadhar_upload_button = tk.Button(aadhar_frame, text="Upload Aadhar Card",command = lambda:upload_docs())
-aadhar_upload_button.pack(side=tk.RIGHT, padx=10)
+aadhar_label.place(x = 250, y = 365)
+#aadhar_file_label = tk.Label(aadhar_frame, text="", font=("Helvetica", 12), bg="#C6E2FF")
+#aadhar_file_label.pack(side=tk.LEFT, padx=10)
+aadhar_upload_button = Button(window, text="Upload Aadhar Card", width = 18, height = 2, command = lambda:upload_docs())
+aadhar_upload_button.pack(side=tk.RIGHT, padx=20)
+aadhar_upload_button.place(x = 400, y = 365)
 
-pan_label = tk.Label(pan_frame, text="PAN Card", font=("Helvetica", 14), bg="#C6E2FF")
+pan_label = tk.Label(window, text="PAN Card", font=("Helvetica", 14), bg="#C6E2FF")
 pan_label.pack(side=tk.LEFT, padx=10)
-pan_file_label = tk.Label(pan_frame, text="", font=("Helvetica", 12), bg="#C6E2FF")
-pan_file_label.pack(side=tk.LEFT, padx=10)
-pan_upload_button = tk.Button(pan_frame, text=" Upload PAN Card",command = lambda:upload_docs())
-pan_upload_button.pack(side=tk.RIGHT, padx=10)
+pan_label.place(x = 250, y = 425)
+#pan_file_label = tk.Label(window, text="", font=("Helvetica", 12), bg="#C6E2FF")
+#pan_file_label.pack(side=tk.LEFT, padx=10)
+pan_upload_button = tk.Button(window, text=" Upload PAN Card", width = 16, height = 2, command = lambda:upload_docs())
+pan_upload_button.pack(side=tk.RIGHT, padx=20)
+pan_upload_button.place(x = 400, y = 425)
 
-passport_label = tk.Label(passport_frame, text="Passport", font=("Helvetica", 14), bg="#C6E2FF")
-passport_label.pack(side=tk.LEFT, padx=10)
-passport_file_label = tk.Label(passport_frame, text="", font=("Helvetica", 12), bg="#C6E2FF")
-passport_file_label.pack(side=tk.LEFT, padx=10)
-passport_upload_button = tk.Button(passport_frame, text=" Upload Passport ",command = lambda:upload_docs())
-passport_upload_button.pack(side=tk.RIGHT, padx=10)
+#passport_label = tk.Label(window, text="Passport", font=("Helvetica", 14), bg="#C6E2FF")
+#passport_label.pack(side=tk.LEFT, padx=10)
+#passport_file_label = tk.Label(window, text="", font=("Helvetica", 12), bg="#C6E2FF")
+#passport_file_label.pack(side=tk.LEFT, padx=10)
+#passport_upload_button = tk.Button(window, text=" Upload Passport ",command = lambda:upload_docs())
+#passport_upload_button.pack(side=tk.RIGHT, padx=10)
 
-button_frame = tk.Frame(content_frame, bg="#C6E2FF")
-button_frame.pack(pady=20, padx=50, fill=tk.X)
+#button_frame = tk.Frame(content_frame, bg="#C6E2FF")
+#button_frame.pack(pady=20, padx=50, fill=tk.X)
 
 # Close Button to exit the application
-close_button = tk.Button(button_frame, text="Close the App", font=("Helvetica", 14), command=root.quit)
+close_button = tk.Button(window, text="Close the KYC Application", font=("Helvetica", 14), command=window.destroy)
 close_button.pack(side=tk.RIGHT,padx=13)
+close_button.place(x = 335, y = 560)
+
 
 # Submit Button
 #show_popup
-submit_button = tk.Button(button_frame, text="Submit", font=("Helvetica", 14), command=submit_docs)
-submit_button.pack(side=tk.RIGHT,padx=11)
-
-# capture Image Button
-captureImage_button = tk.Button(button_frame, text="Capture Image", font=("Helvetica", 14),command = TrackImages)
-captureImage_button.pack(side=tk.RIGHT,padx=11)
-
-root.mainloop()
+submit_button = tk.Button(window, text="Submit", font=("Helvetica", 14), width = 9, height = 1, command=submit_docs)
+submit_button.pack(side=tk.RIGHT,padx=15)
+submit_button.place(x = 420, y = 505)
 
 
+detection_button = tk.Button(window, text="Verify Speech", font=("Helvetica", 14), command= start_speech_recognition)
+detection_button.pack(side=tk.RIGHT,padx=15)
+detection_button.place(x =550, y = 505)
 
 
+# Create a label to display the text to be used for verification 
+text_recog = "I am a responsible human being"
+text_label = tk.Label(window, text= text_recog, font=("Helvetica", 16))
+text_label.pack(padx=20, pady=10)
+
+#Facial Recognition
+RecogImg = tk.Button(window, text ="Capture Image", command = TrackImages,font=("Helvetica", 14), state=tk.DISABLED)
+#font =('times', 15, ' bold '))
+RecogImg.place(x = 250, y = 505)
+
+#List to store the uploaded file paths
+file_names = []
+
+popup_queue = []
+
+window.mainloop()
